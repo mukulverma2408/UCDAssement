492a80ee7b4f7b479dcad235a663ba0c7683f30f
mukulverma2408 <mukulverma2408@gmail.com>
2021-12-16 23:46:22 +0530

    Knn Model Update

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
 # Python Code for UCD Assement
-# Author - Mukul Verma
-# Using this project I would like to compare the two most popular online video streaming service provider
-# in India which is Netflix and Amazon Prime Videos, I will use this project
-# to analyze the content on these two platform
-# the conclusion we can draw from the analysis.
-# Source of Data - Kaggle
-# Netflix Data = https://www.kaggle.com/shivamb/netflix-shows
-# Amazon Prime Video Data = https://www.kaggle.com/shivamb/amazon-prime-movies-and-tv-shows
-# OMDB API Data = http://www.omdbapi.com/
-# PIMA India Diabetes Dataset = https://www.kaggle.com/uciml/pima-indians-diabetes-database
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -16,23 +6,29 @@ import requests
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import GridSearchCV
+
 pd.set_option('display.max_rows', 10)
 pd.set_option('display.max_columns', 10)
+
 
 def create_dateframe(file):
     """This function creates Pandas DataFrame"""
     df = pd.read_csv(file)
     return df
 
+
 def contains_missing_values(dataframe):
     """Function to check if dataframe contains any missing values"""
     missing_val = dataframe.isnull().sum()
     return missing_val
 
+
 def plot_histogram(dataframe):
     """Function to create histogram for dataframe passed"""
     dataframe.isna().sum().plot(kind='bar')
     plt.show()
+
 
 def compare_release_year(dataframe1, dataframe2):
     """Function to create histogram and compare release_year column b/w two dataframe"""
@@ -41,11 +37,13 @@ def compare_release_year(dataframe1, dataframe2):
     plt.legend(['Netflix', 'Amazon'])
     plt.show()
 
-def get_movie(title, year='') :
+
+def get_movie(title, year=''):
     """Function to return movie details from OMDB API, year is an optional argument"""
     API_KEY = 'f92fb218'
-    Info = requests.get('http://www.omdbapi.com/?apikey='+API_KEY+'&t='+title+'&y='+year).json()
+    Info = requests.get('http://www.omdbapi.com/?apikey=' + API_KEY + '&t=' + title + '&y=' + year).json()
     return Info
+
 
 def count_entries(file_name, chunk_size, colname):
     """Function to return a dictionary with counts of occurrences as value for key"""
@@ -58,16 +56,18 @@ def count_entries(file_name, chunk_size, colname):
                 counts_dict[entry] = 1
     return counts_dict
 
+
 def plot_corr(df):
     """Function to plot correlation matrix"""
     corr = df.corr()
     sns.heatmap(corr, annot=True, cmap='viridis', linewidths=.1)
     plt.show()
 
- #Code to demenstorate loading data from csv file
+
+# Code to demenstorate loading data from csv file
 df_netflix = create_dateframe('/home/mukul/PycharmProjects/UCDAssement/data/netflix_titles.csv')
 df_amazon = create_dateframe('/home/mukul/PycharmProjects/UCDAssement/data/amazon_prime_titles.csv')
-#Code to demonstrate basic operations on pandas dataframe like find missing & duplicate
+# Code to demonstrate basic operations on pandas dataframe like find missing & duplicate
 """
 print(df_netflix.info())
 print(df_amazon.info())
@@ -80,7 +80,7 @@ print(df_amazon[duplicates_amazon])
 duplicates_netflix = df_netflix.duplicated(subset=['title'], keep=False)
 print(df_netflix[duplicates_netflix])
 """
- #Code to demenostrate merging dataframe and removing duplicate
+# Code to demonstrate merging dataframe and removing duplicate
 """
 df_combined = pd.concat([df_netflix, df_amazon])
 print(df_combined.info())
@@ -91,16 +91,16 @@ unique_movie = df_combined.drop_duplicates(subset=['title','director','cast'])
 print(unique_movie.info())
 """
 
- #Code to built histogram to check for missing values within two dataframe
-"""
-plot_histogram(df_netflix)
-plot_histogram(df_amazon)
-"""
- #Code to built a histogram to compare release year between two dataset
-"""
-compare_release_year(df_netflix, df_amazon)
-"""
- #Code to demonstrate loading data from API
+# Code to built histogram to check for missing values within two dataframe
+
+# plot_histogram(df_netflix)
+# plot_histogram(df_amazon)
+
+# Code to built a histogram to compare release year between two dataset
+
+# compare_release_year(df_netflix, df_amazon)
+
+# Code to demonstrate loading data from API
 """
 movie_list = ['Fast', 'Jungle', 'Godfather']
 for i in movie_list:
@@ -108,29 +108,31 @@ for i in movie_list:
     for i in movie_info:
         print(i + ': ', movie_info[i])
 """
- #Code to demonstrate creating function, dictionary, using iterators
+# Code to demonstrate creating function, dictionary, using iterators
 """
 result_counts = count_entries('./data/netflix_titles.csv', 10, 'release_year')
 print(result_counts)
 """
- #Code to demonstrate Machine Learning
-diabetes_data =  create_dateframe('/home/mukul/PycharmProjects/UCDAssement/data/diabetes.csv')
-#print(diabetes_data.head())
-#print(diabetes_data.info())
-#print(diabetes_data.describe())
-#From above analysis it's found tha there are some missing values and some outliers.
+# Code to demonstrate Machine Learning
 
-#Replace all missing values with NaN
-diabetes_data[['Glucose','BloodPressure','SkinThickness','Insulin', 'BMI']] = diabetes_data[['Glucose','BloodPressure','SkinThickness','Insulin', 'BMI']].replace(0,np.NaN)
-#print(diabetes_data.describe())
+diabetes_data = create_dateframe('/home/mukul/PycharmProjects/UCDAssement/data/diabetes.csv')
+# print(diabetes_data.head())
+# print(diabetes_data.info())
+# print(diabetes_data.describe())
+# From above analysis it's found tha there are some missing values and some outliers.
+
+# Replace all missing values with NaN
+diabetes_data[['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']] = diabetes_data[
+    ['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI']].replace(0, np.NaN)
+# print(diabetes_data.describe())
 
 total_mising = diabetes_data.isnull().sum().sort_values(ascending=False)
-#print(total_mising)
+# print(total_mising)
 
-#diabetes_data.hist(figsize= (20,20))
-#plt.show()
+# diabetes_data.hist(figsize= (20,20))
+# plt.show()
 
-#Replace NaN values of columns as per distribution
+# Replace NaN values of columns as per distribution
 diabetes_data['Glucose'].fillna(diabetes_data['Glucose'].mean(), inplace=True)
 diabetes_data['BloodPressure'].fillna(diabetes_data['BloodPressure'].mean(), inplace=True)
 diabetes_data['SkinThickness'].fillna(diabetes_data['SkinThickness'].mean(), inplace=True)
@@ -138,13 +140,19 @@ diabetes_data['Insulin'].fillna(diabetes_data['Insulin'].median(), inplace=True)
 diabetes_data['BMI'].fillna(diabetes_data['BMI'].median(), inplace=True)
 
 new_missing = diabetes_data.isnull().sum().sort_values(ascending=False)
-#print('Check if there is any missing data after Imputing')
-#print(new_missing)
+# print('Check if there is any missing data after Imputing')
+# print(new_missing)
 
-# Code to draw a plot and check if there are any corelated values in dataset
-#plot_corr(diabetes_data)
+# Code to draw a plot and check if there are any correlated values in dataset
+# plot_corr(diabetes_data)
+# plt.show()
+
+# Plotting relationship b/w Age & Diabetes
+#sns.countplot(x='Age',hue = 'Outcome', data=diabetes_data)
 #plt.show()
 
+
+#Splitting Data in Train & Test set
 X_train, X_test, y_train, y_test = train_test_split(diabetes_data,diabetes_data['Outcome'], test_size=0.30, random_state=25)
 neighbors = np.arange(1,40)
 test_scores = np.empty(len(neighbors))
@@ -168,5 +176,15 @@ plt.ylabel('Accuracy')
 #Train the model with best value of k
 knn = KNeighborsClassifier(n_neighbors = 14)
 knn.fit(X_train, y_train)
-print(knn.score(X_test, y_test))
+#print(knn.score(X_test, y_test))
+
+# Code to demonstrate HyperParameter Tuning
+param_grid = {'n_neighbors': np.arange(1, 50)}
+knn = KNeighborsClassifier()
+knn_cv = GridSearchCV(knn, param_grid, cv=2)
+knn_cv.fit(X_train, y_train)
+print('Best Params :- ', end='')
+print(knn_cv.best_params_)
+print('Best Score :- ', end='' )
+print(knn_cv.best_score_)
 
